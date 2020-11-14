72b10882e3048aacdd50e58fd4a93ed7a4c29491
Marcin Moryc <m.moryc@protonmail.com>
2020-11-14 16:06:17 +0100

    separate play function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const schedule = require('node-schedule');
 const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
-let channel;
+let mainChannel;
 
 
 client.on('ready', () => {
@@ -14,16 +14,25 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if(message.content == "!setupBarka" && message.author.username == "Dywan"){
-        channel =  message.member.voice.channel;
+        mainChannel =  message.member.voice.channel;
         console.log("setupdone");
+    }
+
+    if(message.content == "!playBarka"){
+        if(message.member.voice.channel != null)
+            playBarka(message.member.voice.channel)
+        else{
+            message.reply("join a voice channel first");
+        }
     }
 })
 
-const papiezowa = schedule.scheduleJob({hour: 21, minute: 37}, async ()=>{
+const papiezowa = schedule.scheduleJob({hour: 21, minute: 37}, () => {playBarka(mainChannel)});
+
+
+async function playBarka(channel){
     const connection = await channel.join();
     connection.play(ytdl('https://www.youtube.com/watch?v=0qzLRlQFFQ4', { filter: 'audioonly' }));
-});
-
-
+}
 
 client.login(process.env.DISCORD_TOKEN);
