e6dfdbb4b3fe54c5c2408bd8dd334e815448d055
Marcin Moryc <m.moryc@protonmail.com>
2020-11-15 16:31:55 +0100

    random memuch and timezone change

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 require('dotenv').config()
 const Express = require('express');
-
 const app = Express();
 
 const Discord = require('discord.js');
 const schedule = require('node-schedule');
 const ytdl = require('ytdl-core');
+const fetch = require('node-fetch');
 
 const client = new Discord.Client();
 let mainChannel;
+const memuchyId="755095473487872021/762744066780954695";
 
-
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+
 });
 
 client.on('message', message => {
@@ -28,9 +30,58 @@ client.on('message', message => {
             message.reply("join a voice channel first");
         }
     }
+    if(message.content == "!randomMemuch"){
+        lots_of_messages_getter(message.channel).then(messages => {
+            console.log(`Received ${messages.size} messages`);
+            filteredMessages = messages.filter(m=>{return m.attachments.size>0});
+            let array = [];
+            filteredMessages.forEach(element => {
+                console.log(element.attachments.first());
+                array.push(element.attachments.first());
+            });
+            var resArr = [];
+            array.filter(function(item){
+                var i = resArr.findIndex(x => x.name == item.name);
+                if(i <= -1){
+                    resArr.push({url: item.url, name: item.name});
+                }
+                return null
+            });
+            console.log(resArr);
+            const file = new Discord.MessageAttachment(resArr[Math.floor(Math.random() * resArr.length)].url);
+            console.log(file);
+            message.channel.send({files: [file]});
+          })
+    }
 })
 
-const papiezowa = schedule.scheduleJob({hour: 21, minute: 37}, () => {playBarka(mainChannel)});
+async function lots_of_messages_getter(channel, limit = 1500) {
+    const sum_messages = [];
+    let last_id;
+
+    while (true) {
+        const options = { limit: 100 };
+        if (last_id) {
+            options.before = last_id;
+        }
+
+        const messages = await channel.messages.fetch(options);
+        console.log(messages);
+        messages.forEach(element => {
+            sum_messages.push(element);
+        });
+        last_id = messages.last().id;
+
+        if (messages.size != 100 || sum_messages >= limit) {
+            break;
+        }
+    }
+
+    return sum_messages;
+}
+
+
+const papiezowa = schedule.scheduleJob({hour: 20, minute: 37}, () => {playBarka(mainChannel)});
 
 
 async function playBarka(channel){
@@ -48,3 +99,6 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
     console.log('Express server listening on port ' + app.get('port'));
   });
+
+
+
