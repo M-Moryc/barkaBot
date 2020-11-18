80762dee0ccb52cc6bbd0246915a91d6330b7aee
Marcin Moryc <m.moryc@protonmail.com>
2020-11-18 11:04:02 +0100

    separate timetable stuff, remove lodash dependency

--- src/command.js
+++ src/command.js
@@ -1,7 +1,6 @@
 const {play, stop} = require('./voice');
 const schedule = require('node-schedule');
-const timeTable = require('./data/timetable.json');
-const _ = require("lodash");
+const {returnTimeTable, getDay} = require('./timetable');
 
 function handleCommand(message){
     message.content = message.content.toLocaleLowerCase();
@@ -21,7 +20,7 @@ const commands = {
     play: (message) => {play(message, commandArray[2])},
     roll: (message) => {roll(message)},
     stop: (message) => {stop(message)},
-    setupbarka: (message) => {schedule.scheduleJob({hour: 20, minute: 37}, () => {commands[playBarka](message)});console.log("setupdone"); message.delete();},
+    setupbarka: (message) => {schedule.scheduleJob({hour: 20, minute: 37}, () => {commands[playbarka](message)});console.log("setupdone"); message.delete();},
     plan: (message) =>{message.reply(returnTimeTable(commandArray[2] || getDay(), message))}
               
 }
@@ -33,68 +32,6 @@ function roll(message){
     else
         message.reply(Math.floor(Math.random()*6 +1));
 } 
-function returnTimeTable(day, message){
-    const userRoles = getUserRoles(message);
-    let activeArray = [];
-      timeTable.map((_class =>{
-        const date = new Date();
-        _class.pierwszyDzien = new Date(_class.pierwszyDzien);
-        if(_class.ostatniDzien == null){
-            _class.ostatniDzien = new Date(2021, 5, 1);
-        }
-        else{
-            _class.ostatniDzien = new Date(_class.ostatniDzien);
-            _class.ostatniDzien.setDate(_class.ostatniDzien.getDate()+1);
-        }
-        _class.dzienTygodnia = _class.dzienTygodnia.toLocaleLowerCase();
-        if(date.getTime() > _class.pierwszyDzien.getTime() && date.getTime() < _class.ostatniDzien.getTime() && _class.dzienTygodnia == day.toLocaleLowerCase()
-            && (_class.typ=="Wykład" || (userRoles.includes((_class.tytul).trim() + _class.grupa)))
-        )
-          activeArray.push(_class);
-      }));
-    activeArray.sort((a, b)=>{
-        return parseFloat(a.poczatek) - parseFloat(b.poczatek);
-    });
-    activeArray = _.uniqWith(activeArray, _.isEqual);
-    let responseMessage = "";
-    let lastLine = "";
-    activeArray.map(_class =>{
-        const responseMessageLine = `${_class.poczatek}-${_class.koniec} ${_class.tytul} gr:${_class.grupa} ${_class.typ}\n`;
-        if(lastLine != responseMessageLine){
-            responseMessage += responseMessageLine;
-            lastLine = responseMessageLine;
-        }
-    });
-    return responseMessage;
-}
-function getDay(){
-    switch(new Date().getDay()){
-        case 1: return "pn";
-        case 2: return "wt";
-        case 3: return "śr";
-        case 4: return "cz";
-        case 5: return "pt";
-    }
-}
-function getUserRoles(message){
-    const userRoles = message.member.roles._roles;
-    userRolesArray = [];
-    userRoles.map(role =>{
-        userRolesArray.push(roles[role.name]);
-    });
-    return userRolesArray;
-}
-
-const roles = {
-    'Grupa 1 SS': "Społeczeństwo sieci1",
-    'Grupa 2 SS': "Społeczeństwo sieci2",
-    'Grupa 3 SS': "Społeczeństwo sieci3",
-    'Filozofia 1': "Podstawy filozofii1",
-    'Filozofia 2': "Podstawy filozofii2",
-    'wds2': "Wstęp do socjologii2",
-    'wds3': "Wstęp do socjologii3"
-}
-
 
 
 module.exports = {handleCommand};
--- src/expressBullshit.js
+++ src/expressBullshit.js
@@ -8,4 +8,5 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
 module.exports = {app};
