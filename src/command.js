664542ce829b1343ebd06d6e70c7ef58a2c82cbb
Marcin Moryc <m.moryc@protonmail.com>
2020-11-25 21:00:22 +0100

    refactor commandhandler part 1

--- src/command.js
+++ src/command.js
@@ -3,37 +3,41 @@ const schedule = require('node-schedule');
 const {returnTimeTable, getDay, stringifyClasses} = require('./timetable');
 const {setResponse} = require('./autoResponses');
 const {autoRespond} = require('./autoResponses');
-const { indexOf } = require('ffmpeg-static');
 
 function handleCommand(message){
     autoRespond(message);
     message.content = message.content.toLocaleLowerCase();
-    commandArray = message.content.match(/(?:[^\s"]+|"[^"]*")+/g);
+    commandArray = message.content.match(/b!\s?[a-z]+/);
+    message.content=message.content.replace(/b!\s?[a-z]+/, '').trim();
+    if(!commandArray)
+        return;
+    const command = commandArray[0].replace('b!', '').trim();
+    params = message.content.split(/\s+/);
     if(!commandArray){
         return;
     }
-    commandArray = commandArray.map(command=>{
-        return command.replace(/"/g,"");
-    })
-    if(commandArray[0] != 'b!')
-        return;
-    if(!commands[commandArray[1]]){
+    if(params){
+        params = params.map(param=>{
+            return param.replace(/"/g,"");
+        });
+    }
+    if(!commands[command]){
         message.reply("unknown command");
         return;
     }
-    commands[commandArray[1]](message);
+    commands[command](message);
         
 }
 
 const commands = {
-    play: (message, param=commandArray[2]) => {play(message, param)},
+    play: (message, param=params[0]) => {play(message, param)},
     roll: (message) => {roll(message)},
     stop: (message) => {stop(message)},
     setupbarka: (message) => {schedule.scheduleJob({hour: 20, minute: 37}, () => {commands[play](message, 'barka')});console.log("setupdone"); message.delete();},
-    plan: (message) =>{plan(message, commandArray.slice(2, commandArray.size))},
-    purge: async (message) =>{purge(message, commandArray[2])},
+    plan: (message) =>{plan(message, params)},
+    purge: async (message) =>{purge(message, params[0])},
     syllabus: (message) =>{message.reply('https://sylabusy.agh.edu.pl/pl/1/1/16/1/1/38/50')},
-    ar: (message) =>{setResponse(message, commandArray[2], commandArray[3])}
+    ar: (message) =>{setResponse(message, params[0], params[1])}
               
 }
 
@@ -61,7 +65,7 @@ function plan(message, params) {
         full=true;
         params.splice(params.indexOf('full'));
     }
-
+    console.log(full);
     const planString = stringifyClasses(returnTimeTable(params[0] || getDay(), message, full));
     ifFiloPlayScifi(message, planString);
     message.reply(planString);
